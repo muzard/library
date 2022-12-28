787c2aa04fced2ffe500fd3af55b4b2849b8fc8d
muzard <arttu.kokki@gmail.com>
2022-12-28 16:37:12 +0200

    define grid areas and template

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ function Book(title, author, pages, readYet) {
   this.readYet = readYet;
 }
 
-function addBookToLibrary() {
-  let book = Book(arguments);
+function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
+const book1 = Book("State", "Plato", 500, false)
+
+const book2 = Book("Rottien Saari", "Jo Nesbø", 700, true)
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
